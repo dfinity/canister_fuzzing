7ff7341f817fb9d738a36885d39251c6b3a26dd5
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2025-07-28 13:49:04 +0000

    add decoder config for canister

--- canisters/decode_candid/src/lib.rs
+++ canisters/decode_candid/src/lib.rs
@@ -1,7 +1,10 @@
 #[export_name = "canister_update decode"]
 pub fn decode() {
+    let mut decoding_config = candid_parser::DecoderConfig::new();
+    decoding_config.set_decoding_quota(2_000_000_000);
+    decoding_config.set_skipping_quota(10_000);
     let bytes = ic_cdk::api::call::arg_data_raw();
-    let _b = candid_parser::IDLArgs::from_bytes(&bytes);
+    let _b = candid_parser::IDLArgs::from_bytes_with_config(&bytes, &decoding_config);
     let instructions = ic_cdk::api::performance_counter(0);
     ic_cdk::api::call::reply_raw(&instructions.to_le_bytes());
 }
--- fuzzers/decode_candid_by_instructions/src/decode_candid_by_instructions_test_input.rs
+++ fuzzers/decode_candid_by_instructions/src/decode_candid_by_instructions_test_input.rs
@@ -2,8 +2,9 @@ use ic_state_machine_tests::StateMachineBuilder;
 use ic_types::{ingress::WasmResult, Cycles};
 use sandbox_shim::sandbox_main;
 use slog::Level;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
+use std::path::PathBuf;
 
 fn read_canister_bytes() -> Vec<u8> {
     let wasm_path = std::path::PathBuf::from(std::env::var("DECODE_CANDID_WASM_PATH").unwrap());
@@ -31,34 +32,43 @@ fn run() {
         )
         .unwrap();
 
-    let bytes = [0_u8; 32];
-    // let bytes = include_bytes!(
-    //     "/ic/rs/canister_fuzzing/decode_candid_by_instructions/crashes/adcd18db92297366"
-    // );
+    // let bytes = [0_u8; 32];
 
-    let result = test.execute_ingress(canister_id, "decode", bytes.to_vec());
+    let paths = fs::read_dir(PathBuf::from(
+        "/home/prodsec-fuzzing/canister_fuzzing/fuzzers/decode_candid_by_instructions/crashes/",
+    ))
+    .unwrap();
+    for (index, path) in paths.enumerate() {
+        let p = path.unwrap().path();
+        println!("{}.  Reading file {:?}", index, p.display());
+        let mut f = File::open(p).unwrap();
+        let mut bytes = Vec::new();
+        f.read_to_end(&mut bytes).unwrap();
 
-    let instructions = match result {
-        Ok(WasmResult::Reply(result)) => {
-            let mut cycles = [0u8; 8];
-            cycles.clone_from_slice(&result[0..8]);
-            u64::from_le_bytes(cycles)
-        }
-        Ok(WasmResult::Reject(message)) => {
-            if message.contains("Canister trapped") {
-                println!("{message}");
+        let result = test.execute_ingress(canister_id, "decode", bytes.to_vec());
+
+        let instructions = match result {
+            Ok(WasmResult::Reply(result)) => {
+                let mut cycles = [0u8; 8];
+                cycles.clone_from_slice(&result[0..8]);
+                u64::from_le_bytes(cycles)
             }
-            0
-        }
-        _ => 0,
-    };
+            Ok(WasmResult::Reject(message)) => {
+                if message.contains("Canister trapped") {
+                    println!("{message}");
+                }
+                0
+            }
+            _ => 0,
+        };
 
-    println!("Instructions used {instructions}");
-    println!("Input length {}", bytes.len());
-    let threshold = instructions / bytes.len() as u64;
-    println!(
-        "Threshold crossed: {}, {}",
-        threshold > 10_000_000,
-        threshold
-    );
+        println!("Instructions used {instructions}");
+        println!("Input length {}", bytes.len());
+        let threshold = instructions / bytes.len() as u64;
+        println!(
+            "Threshold crossed: {}, {}",
+            threshold > 10_000_000,
+            threshold
+        );
+    }
 }
